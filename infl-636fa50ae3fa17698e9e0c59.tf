8ac491fea7ef17cfde91979b473cf55dc79cb75d
radomarina <rado@gofirefly.io>
2022-11-12 15:52:11 +0200

    Codification: aws_ssm_parameter

---
+++ infl-636fa50ae3fa17698e9e0c59.tf
@@ -0,0 +1,17 @@
+resource "aws_ssm_parameter" "prod-flywheel-fetcher-producer-chunks-5a1" {
+  arn   = "arn:aws:ssm:us-east-1:094724549126:parameter/prod-flywheel-fetcher-producer-chunks"
+  name  = "prod-flywheel-fetcher-producer-chunks"
+  type  = "String"
+  value = "REDACTED-BY-FIREFLY"
+}
+
+
+
+
+resource "aws_ssm_parameter" "prod-iacathon-crawler-producer-chunks-7a9" {
+  arn   = "arn:aws:ssm:us-east-1:094724549126:parameter/prod-iacathon-crawler-producer-chunks"
+  name  = "prod-iacathon-crawler-producer-chunks"
+  type  = "String"
+  value = "REDACTED-BY-FIREFLY"
+}
+
